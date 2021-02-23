c4f7f640e6c5b25f633b96bb4a1d420a07026fd4
重田 将人 <masato.0928.j@gmail.com>
2021-02-23 11:20:20 +0900

    クイックソートをinplaceにした

--- sort/quiq_sort.rb
+++ sort/quiq_sort.rb
@@ -1,21 +1,31 @@
 # クイックソート
 # O(NlogN)
-def quit_sort(array)
-    return array if array.count <= 1
+def quit_sort(a, l = 0, r = a.count - 1 )
+    return a if r - l = 1
 
-    pivot = array[0]
-    left = []
-    right = []
+    pivot = a[(l + r) / 2]
+    ll = l
+    rr = r
 
-    (1...array.count).each do |i|
-        if pivot < array[i]
-            right << array[i]
-        else
-            left << array[i]
-        end
+    loop do
+        ll += 1 while a[ll] < pivot
+        rr -= 1 while a[rr] > pivot
+
+        break if ll >= rr
+
+        a = swap(a, ll, rr)
+
+        ll += 1
+        rr -= 1
     end
 
-    quit_sort(left) + [pivot] + quit_sort(right)
+    quit_sort(a, l, rr)
+    quit_sort(a, rr + 1, r)
+end
+
+def swap(a, i, j)
+    a[j], a[i] = a[i], a[j]
+    a
 end
 
 a = [10,3,8,5,7,9,11]
